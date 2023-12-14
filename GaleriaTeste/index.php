2cdfb2e547fa64a9894194e79cf7a4c576871fa8
Yubi <99621379+Yubizin@users.noreply.github.com>
2023-12-14 04:46:32 -0300

    Update index.php
    Melhorei na semântica do HTML, como
    Adicionar um atributo "required" ao campo de upload para garantir que não seja enviado vazio, e melhorei a formatação do código PHP para torná-lo mais legível.

--- GaleriaTeste/index.php
+++ GaleriaTeste/index.php
@@ -1,40 +1,45 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Upload de Imagem</title>
     <style>
         .img {
             width: 300px;
             height: 300px;
         }
+        .image-container {
+            margin-bottom: 20px;
+        }
     </style>
 </head>
 <body>
     <h1>Upload de Imagem</h1>
     <form action="upload.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="file" name="fileToUpload" id="fileToUpload" required>
         <input type="submit" value="Upload Imagem" name="submit">
     </form>
 
     <div id="imagem">
-        <!-- Aqui será exibida a imagem -->
+        <!-- Exibição da imagem carregada -->
         <?php
-            // Verificar se a imagem existe e exibi-la
-            if (file_exists('uploads/imagem.jpg')) {
-                echo '<img src="uploads/imagem.jpg" alt="Imagem">';
+            $caminho_imagem = 'uploads/imagem.jpg';
+            if (file_exists($caminho_imagem)) {
+                echo '<img src="' . $caminho_imagem . '" alt="Imagem" class="img">';
             }
         ?>
     </div>
+
     <h1>Galeria de Imagens</h1>
 
     <!-- Exibição de todas as imagens carregadas -->
     <div class="gallery">
         <?php
-        $diretorio = "uploads/";
-        $imagens = scandir($diretorio);
+            $diretorio = "uploads/";
+            $imagens = array_diff(scandir($diretorio), array('..', '.'));
 
-        foreach ($imagens as $imagem) {
-            if ($imagem != '.' && $imagem != '..') {
+            foreach ($imagens as $imagem) {
                 $caminho_imagem = $diretorio . $imagem;
 
                 if (is_file($caminho_imagem)) {
@@ -45,28 +50,26 @@
                     echo '</div>';
                 }
             }
-        }
 
-        if (count($imagens) <= 2) {
-            echo 'Nenhuma imagem foi carregada ainda.';
-        }
+            if (count($imagens) === 0) {
+                echo 'Nenhuma imagem foi carregada ainda.';
+            }
         ?>
     </div>
-<?php
-if (isset($_GET['imagem'])) {
-    $imagem_a_excluir = $_GET['imagem'];
-    $caminho_imagem = 'uploads/' . $imagem_a_excluir;
 
-    if (file_exists($caminho_imagem)) {
-        // Excluir a imagem
-        unlink($caminho_imagem);
-        echo 'Imagem "' . $imagem_a_excluir . '" foi excluída com sucesso.';
-    } else {
-        echo 'A imagem não existe ou já foi excluída anteriormente.';
-    }
-}
-?>
-    </div>
+    <?php
+        if (isset($_GET['imagem'])) {
+            $imagem_a_excluir = $_GET['imagem'];
+            $caminho_imagem = 'uploads/' . $imagem_a_excluir;
+
+            if (file_exists($caminho_imagem)) {
+                // Excluir a imagem
+                unlink($caminho_imagem);
+                echo 'Imagem "' . $imagem_a_excluir . '" foi excluída com sucesso.';
+            } else {
+                echo 'A imagem não existe ou já foi excluída anteriormente.';
+            }
+        }
+    ?>
 </body>
 </html>
-
